1938fddd3604dcd3e9698d2e02df0d50fce9031b
Bruce Ussenco <bruceussenco@gmail.com>
2024-05-24 20:35:30 -0300

    replace renderTiles func by renderChunk, change map by chunk and store chunk in a single string

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ canvas.width  = 400;
 canvas.height = 400;
 canvas.addEventListener("mousemove", updateMousePos, false);
 
-const bgColor = "#4488ee";
-const tileSize = 20;
+const bgColor   = "#4488ee";
+const chunkSize = 16;
+const tileSize  = 20;
 
 const mouse  = {x: 0, y: 0};
 const camera = {x: 0, y: 0};
@@ -25,7 +26,7 @@ function gameLoop(timeStamp) {
     camera.x = mouse.x;
     camera.y = mouse.y;
     render(camera);
-    renderFPS(fps);
+    showFPS(fps);
     window.requestAnimationFrame(gameLoop);
 }
 function update(dt) {
@@ -39,7 +40,7 @@ function render(camera) {
     ctx.translate(-camera.x, -camera.y);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     renderBg(camera);
-    renderTiles();
+    renderChunk();
     ctx.restore();
 }
 
@@ -52,15 +53,14 @@ function renderBg(camera) {
         canvas.height
     );
 }
-function renderTiles() {
+function renderChunk() {
     ctx.fillStyle   = "#aa5555";
     ctx.strokeStyle = "#000000";
 
-    for (let j = 0; j < map.length; j++) {
-        const line = map[j];
-        for (let i = 0; i < line.length; i++) {
-            const tile = line[i];
-            if (tile == '#') {
+    for (let j = 0; j < chunkSize; j++) {
+        for (let i = 0; i < chunkSize; i++) {
+            const index = j * chunkSize + i;
+            if (chunk[index] == '#') {
                 const x = i * tileSize;
                 const y = j * tileSize;
                 ctx.fillRect(x, y, tileSize, tileSize);
@@ -69,7 +69,7 @@ function renderTiles() {
         }
     }
 }
-function renderFPS(fps) {
+function showFPS(fps) {
     ctx.font = "20px Arial";
     ctx.fillStyle = "#00ff88";
     ctx.fillText("FPS: " + fps, 10, 30);
